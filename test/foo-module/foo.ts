fff5c26b0f4ec3f3ba3e5b80089af3ef6798d5ce
Allison Machado <aallison93@gmail.com>
2022-07-20 18:10:45 +0100

    fix: lint test files (#4)

--- test/foo-module/foo.ts
+++ test/foo-module/foo.ts
@@ -1,11 +1,11 @@
-import { Foo } from "../../src/foo-module";
-import { expect } from "chai";
+import { Foo } from '../../src/foo-module';
+import { expect } from 'chai';
 
-describe("Foo module", function() {
-    describe("exposed initialization", function() {
-        it("should receive proper size", function() {
-            const foo = new Foo(5);
-            expect(foo.getSize()).to.equal(5);
-        });
+describe('Foo module', function() {
+  describe('exposed initialization', function() {
+    it('should receive proper size', function() {
+      const foo = new Foo(5);
+      expect(foo.getSize()).to.equal(5);
     });
+  });
 });
